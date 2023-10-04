b1448634e65e41b5ba28a33ce187ea49f2d9c78b
ChoiEunCheol <ChoiEunCheol>
2023-10-04 12:12:49 +0900

    기초적인 서버 구축 localhost:8080으로 들어가고 ctrl+c로 종료한다.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("ㅎㅇ");
+const http = require('http');
+
+http.createServer(function(request, response){
+    let writeHeadObject ={
+        'Content-Type': 'text/html'
+    }
+    
+    response.writeHead(200,writeHeadObject);
+    response.end("hello")
+    
+}).listen(8080);
+
